e88d45d736a1971f6f7171477f7328f7e8ce26cd
mason-phenomx <83932136+msonlim@users.noreply.github.com>
2023-02-13 19:28:33 +0900

    applied: swinvit.py(swinMSA & window_reverse method: N2C)

--- Net/ViT_fsCNN/swinvit.py
+++ Net/ViT_fsCNN/swinvit.py
@@ -11,66 +11,75 @@ import numpy as np
 
 
 ### Patch & Positional Embedding
-class EMB(nn.Module):
+class swinEMB(nn.Module):
     def __init__(self, params):
-        super(EMB, self).__init__()
+        super(swinEMB, self).__init__()
         
-        self.patch_size = int(params['patch_size']*2) 
+        self.dim = int(params['dimension'])
+
+        self.patch_size = int(params['patch_size']) 
         self.input_c = int(params['num_channels'])
         self.emb_size = int(params['emb_size']) 
         self.img_hwd = int(params['img_hwd'])
 
         self.dropout_rate = params['drop_rate']
 
-        self.cls_token = nn.Parameter(torch.randn(1, 1, self.emb_size)) ### class token '+1'
-        self.emb_pos = nn.Parameter(torch.randn((self.img_hwd//self.patch_size)**3, self.emb_size)) # hwd/p^3 '+1', embedding size
+        self.emb_pos = nn.Parameter(torch.empty((self.img_hwd//self.patch_size)**int(self.dim), self.emb_size))
+        nn.init.kaiming_normal_(self.emb_pos, nonlinearity='relu') # replaced Weight Initialization code suggested by Kaiming He's
 
-        self.conv_layer1 = nn.Conv3d(in_channels=self.input_c, out_channels=self.emb_size, kernel_size=self.patch_size, stride=self.patch_size)
+
+        if self.dim == 3: 
+            self.conv_layer1 = nn.Conv3d(in_channels=self.input_c, out_channels=self.emb_size, kernel_size=self.patch_size, stride=self.patch_size)
+            self.flatten_layer1 = nn.Flatten(start_dim=2, end_dim=-1) 
+        else: 
+            self.conv_layer1 = nn.Conv2d(in_channels=self.input_c, out_channels=self.emb_size, kernel_size=self.patch_size, stride=self.patch_size)
+            self.flatten_layer1 = nn.Flatten(start_dim=1, end_dim=-1) 
+
         self.dropout_layer1 = nn.Dropout(self.dropout_rate)
 
+
     def forward(self, x):
-        b, c, h, w, d = x.shape # 5D
+        b, c, h, w, d = x.shape 
 
         x = self.conv_layer1(x)
-        x = rearrange(x, 'b e (h) (w) (d) -> b (h w d) e')  # x = x.reshape(b, -1, self.emb_size).transpose(1, 2)
-        
+        x = self.flatten_layer1(x).transpose(1, 2)
+
         x += self.emb_pos
-        x = self.dropout_layer1(x)  # added
+        x = self.dropout_layer1(x)
         
         return x
 
 
-### Multi-head Self Attention
-class MSA(nn.Module):
-    def __init__(self, params):
-        super(MSA, self).__init__()
+### Multi-head Self Attention >> must be modified
+class swinMSA(nn.Module):
+    def __init__(self, params, bias=True):
+        super(swinMSA, self).__init__()
 
         self.emb_size = int(params['emb_size'])
         self.num_heads = int(params['num_heads'])
         self.dropout_rate = float(params['drop_rate'])
 
-        self.K = nn.Linear(self.emb_size, self.emb_size)#, self.emb_size)
-        self.Q = nn.Linear(self.emb_size, self.emb_size)#, self.emb_size)
-        self.V = nn.Linear(self.emb_size, self.emb_size)#, self.emb_size)
+        self.Q = nn.Linear(self.emb_size, self.emb_size, bias=bias)
+        self.K = nn.Linear(self.emb_size, self.emb_size, bias=bias)
+        self.V = nn.Linear(self.emb_size, self.emb_size, bias=bias)
  
         self.drop_layer1 = nn.Dropout(self.dropout_rate)
         self.proj_layer1 = nn.Linear(self.emb_size, self.emb_size)
 
 
-    def forward(self, x, mask: torch.Tensor=None):
-        Q = rearrange(self.Q(x), "b n (h d) -> b h n d", h=self.num_heads)
-        K = rearrange(self.K(x), "b n (h d) -> b h n d", h=self.num_heads)
-        V = rearrange(self.V(x), "b n (h d) -> b h n d", h=self.num_heads)
+    def forward(self, x):
+        Q = rearrange(self.Q(x), "b n (h d) -> b n h d", h=self.num_heads)
+        K = rearrange(self.K(x), "b n (h d) -> b n h d", h=self.num_heads)
+        V = rearrange(self.V(x), "b n (h d) -> b n h d", h=self.num_heads)
 
+        scale = np.sqrt(self.emb_size)
         energy = torch.einsum('bhqd, hbkd -> bhqk', Q, K)
-        if mask != None: energy.mask_fill(~mask, torch.finfo(torch.float32).min)
 
-        scale = self.emb_size ** (1/2)
-        att1 = F.softmax(energy, dim=1) / scale
+        att1 = F.softmax(energy, dim=2) / scale
         att2 = self.drop_layer1(att1)
 
         x = torch.einsum('bhal, bhlv -> bhav', att2, V)
-        x = rearrange(x, 'b h n d -> b n (h d)')
+        x = rearrange(x, 'b n h d -> b n (h d)')
 
         out = self.proj_layer1(x)
 
@@ -93,34 +102,148 @@ class MLP(nn.Module):
 
 
     def forward(self, x):
-        x = self.linear_layer1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.linear_layer2(x)
+        pass
+        # x = self.linear_layer1(x)
+        # x = self.relu(x)
+        # x = self.dropout(x)
+        # x = self.linear_layer2(x)
 
+        # return x
+
+
+# class SwinBasicLayer(nn.Module):
+#     '''
+#     {SwinTransformerBlock, Downsample} * depth
+#     SwinTransformerBlock(N % 2 == 1): -> shift_size = window_size // 2
+#     Downsample: Patch Merging (Decreasing Number of Patches)
+#     '''
+
+    ### devide window for a different shape(shifted window)
+    # def window_partition(x, window_size):
+    #     x_shape = x.size()
+    #     if len(x_shape) == 5: #3D shape
+    #         b, d, h, w, c = x_shape
+    #         # x.size = 8
+    #         x = x.view(b, 
+    #                    d // window_size[0], window_size[0], 
+    #                    h // window_size[1], window_size[1], 
+    #                    w // window_size[2], window_size[2], 
+    #                    c)
+
+    #         windows_total = window_size[0] * window_size[1] * window_size[2]
+            
+    #         windows = x.permute(0, 1, 3, 5, 2, 4, 6, 7).contiguous().view(-1, windows_total, c) # reshape >> no need contiguous()
+
+    #     elif len(x_shape) == 4: #2D shape
+    #         b, d, h, w = x_shape
+    #         # x.size = 6
+    #         x = x.view(b, h // window_size[0], window_size[0],
+    #                 w // window_size[1], window_size[1], c)
+            
+    #         windows_total = window_size[0] * window_size[1]
+
+    #         windows = x.permute(0, 1, 3, 2, 4, 5).contigous().view(-1, windows_total, c)
+
+    #     return windows
+
+
+    # def window_reverse(x, window_size, dims):
+    #     if len(dims) == 4:
+    #         b, d, h, w = dims
+    #         x = x.view(b,
+    #                    d // window_size[0], h // window_size[1], w // window_size[2],
+    #                    window_size[0], window_size[1], window_size[2],
+    #                    -1,)
+
+    #         windows = x.permute(0, 1, 4, 2, 5, 3, 6, 7).contiguous().view(b, d, h, w, -1)
+
+    #     elif len(dims) == 3:
+    #         b, h, w = dims
+    #         x = x.view(b, 
+    #                    h // window_size[0], w // window_size[1], 
+    #                    window_size[0], window_size[1], 
+    #                    -1)
+    #         windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(b, h, w, -1)
+
+    #     return windows
+
+### N-Stack structure, input reshaping + windows partition + self attention layer
+### Must be changed to a N-Stack loop form
+class SwinTransformerBlock(nn.Module):
+    def __init__(self, params):
+        super(SwinTransformerBlock, self).__init__()
+        
+        self.dim = int(params['dimension'])
+        self.patch_size = int(params['patch_size']) 
+
+        self.input_c = int(params['num_channels'])
+        self.emb_size = int(params['emb_size']) 
+        self.img_hwd = int(params['img_hwd'])
+
+        self.dropout_rate = params['drop_rate']
+
+        self.hwd = self.img_hwd // self.patch_size
+        self.windows = [8, 8, 8] # window size, need parameterize
+
+        self.EMB = swinEMB(params) # Patch Embedding + Flatten Block
+        self.MSA = swinMSA(params) # Multi-head Self Attention Block
+
+
+    def window_partition(self, x, window_size): # window size must be gathered from the outside(N-Stack)
+        if len(x.shape) == 5: #3D shape
+            b, d, h, w, c = x.shape
+            # x.shape = 8
+            x = x.view(b, 
+                       d // window_size[0], window_size[0], 
+                       h // window_size[1], window_size[1], 
+                       w // window_size[2], window_size[2], 
+                       c)
+
+        else: #2D shape
+            b, d, h, w = x.shape
+            # x.shape = 6
+            x = x.view(b, h // window_size[0], window_size[0],
+                    w // window_size[1], window_size[1], c)
+            
+        windows_total = window_size[0] * window_size[1] * window_size[2] if len(x.shape) == 5 else window_size[0] * window_size[1]
+        windows = x.permute(0, 1, 3, 2, 4, 5).contigous().view(-1, windows_total, c)if len(x.shape) == 5 else x.permute(0, 1, 3, 5, 2, 4, 6, 7).contiguous().view(-1, windows_total, c)
+
+        return windows
+
+
+    def window_reverse(self, x, window_size): # , dims
+        if len(x.shape) == 4:
+            b, d, h, w = x.shape
+            x = x.view(b, d // window_size[0], h // window_size[1], w // window_size[2],
+                          window_size[0], window_size[1], window_size[2], -1)
+
+            windows = x.permute(0, 1, 4, 2, 5, 3, 6, 7).contiguous().view(b, d, h, w, -1)
+
+        elif len(x.shape) == 3:
+            b, h, w = x.shape
+            print(b, h, w)
+            x = x.view(b, h // window_size[0], w // window_size[1], 
+                          window_size[0], window_size[1], -1)
+
+            windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(b, h, w, -1)
+
+        return windows
+
+
+    def forward(self, x):
+        x = self.EMB(x)
+        x = self.window_partition(x.view(1, self.hwd, self.hwd, self.hwd, self.emb_size), self.windows) # change the value 1 to batch_size (future works)
+        x = self.MSA(x)
+        print(x.shape)
+        x = self.window_reverse(x, self.windows)
+
+        print(x.shape)
         return x
 
 
-### devide window for a different shape(shifted window)
-def window_partition(x, window_size):
-    x_shape = x.size()
-    if len(x_shape) == 5: #3D shape
-        b, d, h, w, c = x_shape
-        # x.size = 8
-        x = x.view(b, d // window_size[0], window_size[0], 
-                   h // window_size[1], window_size[1], 
-                   w // window_size[2], window_size[2], c)
+class PatchMerging(nn.Module): # Downsample
+    def __init__(self, params):
+        pass
 
-        windows_total = window_size[0] * window_size[1] * window_size[2]
-        windows = x.permute(0, 1, 3, 5, 2, 4, 6, 7).contiguous().view(-1, windows_total, c) # reshape >> no need contiguous()
-
-    elif len(x_shape) == 4: #2D shape
-        b, d, h, w = x_shape
-        # x.size = 6
-        x = x.view(b, h // window_size[0], window_size[0],
-                   w // window_size[1], window_size[1], c)
-        
-        windows_total = window_size[0] * window_size[1]
-        windows = x.permute(0, 1, 3, 2, 4, 5).contigous().view(-1, windows_total, c)
-
-    return windows
+    def forward(self, x):
+        pass
